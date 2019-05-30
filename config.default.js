4f7941e122a1bb9e88cb8a67508fde2f803f8b61
YzzHa <18390955482@163.com>
2019-05-31 00:20:02 +0800

    init

---
+++ config.default.js
@@ -0,0 +1,5 @@
+module.exports = {
+    oapiHost: 'https://oapi.dingtalk.com',
+    appkey: 'dingygf5ggra9amm7glv', 
+    appsecret: 'TJIieBvHTy7LksaCA7Vd6vXhi6bFvwmtb2_hxW9j_x2F6Soc_nSNu4vSm17pIexm'
+}
